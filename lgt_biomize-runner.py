3edaaa7a1c339fc184bfc6adaefb1b90054cfff6
Christian Werner <christian.werner@senckenberg.de>
2017-11-08 16:42:49 +0100

    Misc changes: bugfix, updated runner, cli
    Bit of both... A cli interface for lgt_biomize is added. Also, a few
    bugs in the EarthShape biomization are corrected now...

--- lgt_biomize-runner.py
+++ lgt_biomize-runner.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Convenience wrapper for running lgt_convert directly from source tree."""
 
-from lpjguesstools.lgt_biomize.lgt_biomize import main
+from lpjguesstools.lgt_biomize.cli import cli
 
 if __name__ == '__main__':
-    main()
+    cli()
